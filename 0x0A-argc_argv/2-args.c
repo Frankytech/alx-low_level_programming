f2141d483a97584592af0374f6c0b654de7c9fb3
frankytech <emenikefc16082@gmail.com>
2022-10-02 13:11:34 -0700

    sense commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/*
+ * main : prints the arguments passed to it
+ * argc : nnumber of arguments
+ * argv: array of arguments string
+ *
+ *return 0
+ *
+ */
+
+int main(int argc char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%d\n", argv[i]);
+
+	}
+	return (0);
+}
