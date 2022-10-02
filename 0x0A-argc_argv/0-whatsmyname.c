2d9eb3c46e83a35e908d9df0e5abbcfc69e1710d
frankytech <emenikefc16082@gmail.com>
2022-10-01 23:08:26 -0700

    my commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main prints the name of the file followed a by a new line 
+ * and if you rename the program , it wold printits new name 
+ */
+
+int main(int  argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return 0;
+}
