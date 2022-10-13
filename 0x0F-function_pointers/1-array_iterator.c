499f3b9a93dedbac86795d62431bb3973cbfb0f4
frankytech <emenikefc16082@gmail.com>
2022-10-12 19:42:07 -0700

    my commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/*
+ *array iterator executes a function given as a parameter
+ *on each element of an array
+ *
+ *@array - array to iterate
+ *@size -size of the array
+ *@action - pointer to the array iterator
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+{
+	if (array == NULL || action == NULL)
+		return;
+	while (size--)
+	{
+		(*action)(*array++)
+	}
+
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+a repository on funtion pointers , contains task that covers the diffrent applications of function pointers
