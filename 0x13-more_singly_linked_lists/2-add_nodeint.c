a5bb551c8211c9060354523f082c4e6a15a9137c
frankytech <emenikefc16082@gmail.com>
2022-10-25 11:53:38 +0000

    update om my task 0, 1 and 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "list.h"
+
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list.
+ *
+ * @head: pointer to the addresss of the head of thye listint_ list 
+ * @n: the integer for tthe new node to contain 
+ * return: if the function faikls - NULL 
+ * otherwise - the address of the new element 
+ */
+listint_t *add_nodeint(listint_t **head, const int n);
+{
+	listint_t *new;
+
+	new = malloc (sizeof(listint_t));
+	if (new == NULL)
+			return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -5,11 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct list_s
+typedef struct listint_s
 {
-	 char *str;
-	 unsigned int len;
-	 struct list_s *next;
+	 int n;
+	 struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -34,4 +33,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
-#endif
+listint_t *reverse_listint(listint_t **head);
+
+size_t print_listint_safe(const listint_t *head)
+
+size_t free_listint_safe(listint_t **h);
+
+listint_t *find_listint_loop(listint_t *head);
+
+#endif /* LIST_H */
